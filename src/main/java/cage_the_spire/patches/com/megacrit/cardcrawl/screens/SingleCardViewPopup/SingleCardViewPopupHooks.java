672a823cd3305ce7c6ce24007db13599f6947e31
alexdriedger <aldriedger@gmail.com>
2018-06-16 21:33:25 -0500

    Big cards render nick cage picture

---
+++ src/main/java/cage_the_spire/patches/com/megacrit/cardcrawl/screens/SingleCardViewPopup/SingleCardViewPopupHooks.java
@@ -0,0 +1,56 @@
+package cage_the_spire.patches.com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+
+import cage_the_spire.patches.com.megacrit.cardcrawl.cards.AbstractCard.AbstractCardHooks;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+
+import java.lang.reflect.Field;
+
+public class SingleCardViewPopupHooks {
+
+    private static final String NICK_CAGE_FACE_LARGE_PATH = "nick_cage_face_large.png";
+
+    public static void setPortraitImg(SingleCardViewPopup instance) {
+        try {
+            Field portraitImg = SingleCardViewPopup.class.getDeclaredField("portraitImg");
+            portraitImg.setAccessible(true);
+            portraitImg.set(instance, ImageMaster.loadImage(NICK_CAGE_FACE_LARGE_PATH));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.out.println("Could not change big card card artwork");
+        }
+    }
+
+    @SpirePatch(
+            cls = "com.megacrit.cardcrawl.screens.SingleCardViewPopup",
+            method = "open",
+            paramtypes = {
+                    "com.megacrit.cardcrawl.cards.AbstractCard"
+            }
+    )
+    public static class PostOpenFirstHook {
+        public static void Postfix(Object __obj_instance, AbstractCard card) {
+            System.out.println("Post open(AbstractCard card)");
+            setPortraitImg((SingleCardViewPopup) __obj_instance);
+        }
+    }
+
+    @SpirePatch(
+            cls = "com.megacrit.cardcrawl.screens.SingleCardViewPopup",
+            method = "open",
+            paramtypes = {
+                    "com.megacrit.cardcrawl.cards.AbstractCard",
+                    "com.megacrit.cardcrawl.cards.CardGroup"
+            }
+    )
+    public static class PostOpenSecondHook {
+        public static void Postfix(Object __obj_instance, AbstractCard card, CardGroup group) {
+            System.out.println("Post open(AbstractCard card, CardGroup group)");
+            setPortraitImg((SingleCardViewPopup) __obj_instance);
+        }
+    }
+}
