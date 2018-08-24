7d07bf575cb82bc842a02c006c95748d66272fda
alexdriedger <aldriedger@gmail.com>
2018-08-24 15:06:19 -0500

    custom mod is custom mode

--- src/main/java/cage_the_spire/NicolasCageCustomMod.java
+++ src/main/java/cage_the_spire/NicolasCageCustomMod.java
@@ -2,8 +2,12 @@ package cage_the_spire;
 
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.screens.custom.CustomMod;
+import org.lwjgl.Sys;
+
+import java.lang.reflect.Field;
 
 public class NicolasCageCustomMod extends CustomMod {
 
@@ -22,6 +26,15 @@ public class NicolasCageCustomMod extends CustomMod {
         this.description = MOD_DESCRIPTION;
         this.hb = new Hitbox(1000.0F * Settings.scale, 90.0F * Settings.scale);
         this.isDailyMod = true; // So mod can be detected in CustomTrial
+        try {
+            Field label = CustomMod.class.getDeclaredField("label");
+            label.setAccessible(true);
+            label.set(this, (FontHelper.colorString(new StringBuilder().append("[").append(this.name).append("]").toString(), color) + " " + this.description));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.out.println("Could not change custom mod label to nicolas cage");
+        }
     }
 
     public static boolean isActive() {
