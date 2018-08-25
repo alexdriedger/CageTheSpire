364d7f9b72dc1231430a64a674e6984d6345eac8
alexdriedger <aldriedger@gmail.com>
2018-08-25 17:42:01 -0500

    Removed unused hook

--- src/main/java/cage_the_spire/CageTheSpire.java
+++ src/main/java/cage_the_spire/CageTheSpire.java
@@ -1,16 +1,19 @@
 package cage_the_spire;
 
 import basemod.BaseMod;
+import basemod.interfaces.EditCustomModeModsSubscriber;
 import basemod.interfaces.EditStringsSubscriber;
 import com.badlogic.gdx.Gdx;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.RunModStrings;
+import com.megacrit.cardcrawl.screens.custom.CustomMod;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @SpireInitializer
-public class CageTheSpire implements EditStringsSubscriber {
+public class CageTheSpire implements EditStringsSubscriber, EditCustomModeModsSubscriber {
 
     public static final String CAGE_COSMETIC_ID = "Cage Cosmetic";
     public static final String CAGE_FULL_EFFECT_ID = "Cage The Spire";
@@ -28,6 +31,12 @@ public class CageTheSpire implements EditStringsSubscriber {
         String modString = Gdx.files.internal("localization/CageTheSpire-run_mods.json").readString(
                 String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(RunModStrings.class, modString);
+    }
+
+    @Override
+    public void receiveCustomModeMods(List<CustomMod> list) {
+        list.add(0, new CustomMod(CageTheSpire.CAGE_COSMETIC_ID, "p", true));
+        list.add(1, new CustomMod(CageTheSpire.CAGE_FULL_EFFECT_ID, "p", true));
     }
 
     public static boolean isCageCosmeticActive() {
